cc320e9f982801f1b1b2e5b6c56bc2f9114463fd
root <root@app.cachoid.com>
2016-11-23 12:06:31 -0500

    First check-in

---
+++ curl_api.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+urlencode() {
+    # urlencode <string>
+    old_lc_collate=$LC_COLLATE
+    LC_COLLATE=C
+    
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+    
+    LC_COLLATE=$old_lc_collate
+}
+
+urldecode() {
+    # urldecode <string>
+
+    local url_encoded="${1//+/ }"
+    printf '%b' "${url_encoded//%/\\x}"
+}
+
+# set -x;
+
+if [ ${#} -lt '2' ];
+then
+	echo "$0 <token> <fqdn> [<subpage>]";
+else
+	if [ ${#} -eq '2' ];
+	then
+		curl -XGET -vvv -H "Accept: application/vnd.cachoid.v1+json" -H "Authorization: Bearer $1" https://api.cachoid.com/cache/status/$(urlencode $2)
+	else
+		curl -XGET -vvv -H "Accept: application/vnd.cachoid.v1+json" -H "Authorization: Bearer $1" https://api.cachoid.com/cache/status/$(urlencode $2)/$(urlencode $3)
+	fi
+fi
